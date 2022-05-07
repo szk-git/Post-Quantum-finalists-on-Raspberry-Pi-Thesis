c967da1e507270d1ee6428998eb05e5fff39a06d
Kristof Szabo <aisdos007@gmail.com>
2022-05-07 17:03:28 +0100

    backend with k8s

--- start.sh
+++ start.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-cd backend/
-docker rmi django-backend:1.0
-echo "========================================================="
-docker build -t django-backend:1.0 .
-echo "========================================================="
-
-echo "========================================================="
-kubectl create ns jfirbj-thesis
-echo "========================================================="
-
-echo "========================================================="
-kubectl run django-backend --image=django-backend:1.0 --port=8000 -n jfirbj-thesis
-echo "========================================================="
-
-cd ..
-
 echo "DONE"
