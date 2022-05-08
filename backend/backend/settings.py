7fd4a7d25ba136a9ca53dd041f26ed54d2bbfc1a
Kristof Szabo <aisdos007@gmail.com>
2022-05-08 12:45:46 +0100

    update backend Dockerfile and settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--45nb89a^afa0@g+pab2!as!hf5ivy0!!9^p&fr8n*--_np#3y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.100.25']
+ALLOWED_HOSTS = ['192.168.100.25', '*']
 
 
 # Application definition
