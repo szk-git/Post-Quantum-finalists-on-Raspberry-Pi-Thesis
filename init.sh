af4fe55352ef73486804cd6b74835efd3987d6d9
Kristóf Szabó <aisdos007@gmail.com>
2022-05-24 21:13:37 +0200

    update

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "======================= SYSTEM UNDER UPDATING ======================="
 sudo apt update
 echo "======================= INSTALL <libssl-dev> PACKAGE ======================="
@@ -13,3 +14,7 @@ echo "======================= INSTALL <nodejs> PACKAGE ======================="
 sudo apt install nodejs -y
 echo "======================= INSTALL <npm> PACKAGE ======================="
 sudo apt install npm -y
+
+mv database.mv.db ~
+
+(cd frontend/; npm install)
